4333cef1d6431725d4255deb70ea1d5ae953f827
wanm <1097736928@qq.com>
2019-02-28 15:51:24 +0800

    三次提交 项目打包上线

---
+++ src/main/java/com/sxt/SpringBootStartApplication.java
@@ -0,0 +1,18 @@
+//package com.sxt;
+//
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//
+///**
+// * @author WanMing
+// * @date 2019/2/27 20:46
+// */
+//
+//public class SpringBootStartApplication extends SpringBootServletInitializer {
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//
+//        return builder.sources(WmsApplication.class);
+//    }
+//}
